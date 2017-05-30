75c5576489d42c85afe1200748a068d3c780932d
Dawid Senko <dawid.senko@sb-betting.com>
2017-05-30 15:18:10 +0200

    + added serializeObject.js new method

--- dist/spike-framework.js
+++ dist/spike-framework.js
@@ -6743,7 +6743,7 @@ app.rest = {
  */
 jQuery.fn.extend({
 
-  serializeObject: function () {
+  serializeObject: function (omitNumberParsingFields) {
 
     var formSelector = $(this);
     var serializedObject = {};
@@ -6759,7 +6759,46 @@ jQuery.fn.extend({
         value = false;
       }
 
-      serializedObject[name] = app.util.System.tryParseNumber(value);
+      if(omitNumberParsingFields && omitNumberParsingFields.indexOf(name) > -1){
+        serializedObject[name] = value;
+      }else{
+        serializedObject[name] = app.util.System.tryParseNumber(value);
+      }
+
+    };
+
+    formSelector.find('input[name]').each(serializeField);
+    formSelector.find('select[name]').each(serializeField);
+    formSelector.find('textarea[name]').each(serializeField);
+
+    formSelector.find('input[type="checkbox"][name]').each(function () {
+
+      var name = $(this).attr('name');
+      serializedObject[name] = $(this).is(':checked');
+
+    });
+
+    return serializedObject;
+
+  },
+
+  serializeObjectWithoutNumberParsing: function () {
+
+    var formSelector = $(this);
+    var serializedObject = {};
+
+    var serializeField = function(){
+
+      var value = $(this).val();
+      var name = $(this).attr('name');
+
+      if (value == 'on') {
+        value = true;
+      } else if (value == 'off') {
+        value = false;
+      }
+
+      serializedObject[name] = value;
 
     };
 
--- src/jquery/serializeObject.js
+++ src/jquery/serializeObject.js
@@ -9,7 +9,7 @@
  */
 jQuery.fn.extend({
 
-  serializeObject: function () {
+  serializeObject: function (omitNumberParsingFields) {
 
     var formSelector = $(this);
     var serializedObject = {};
@@ -25,7 +25,46 @@ jQuery.fn.extend({
         value = false;
       }
 
-      serializedObject[name] = app.util.System.tryParseNumber(value);
+      if(omitNumberParsingFields && omitNumberParsingFields.indexOf(name) > -1){
+        serializedObject[name] = value;
+      }else{
+        serializedObject[name] = app.util.System.tryParseNumber(value);
+      }
+
+    };
+
+    formSelector.find('input[name]').each(serializeField);
+    formSelector.find('select[name]').each(serializeField);
+    formSelector.find('textarea[name]').each(serializeField);
+
+    formSelector.find('input[type="checkbox"][name]').each(function () {
+
+      var name = $(this).attr('name');
+      serializedObject[name] = $(this).is(':checked');
+
+    });
+
+    return serializedObject;
+
+  },
+
+  serializeObjectWithoutNumberParsing: function () {
+
+    var formSelector = $(this);
+    var serializedObject = {};
+
+    var serializeField = function(){
+
+      var value = $(this).val();
+      var name = $(this).attr('name');
+
+      if (value == 'on') {
+        value = true;
+      } else if (value == 'off') {
+        value = false;
+      }
+
+      serializedObject[name] = value;
 
     };
 
